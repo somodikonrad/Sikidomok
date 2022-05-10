99cd636768f024dc65af1c3e1b779b6194d4a098
Valentin0216 <simonvalentinmark@turr.hu>
2022-05-10 11:24:06 +0200

    Főablak alapkonfigurálása

--- sikidomok.py
+++ sikidomok.py
@@ -1,6 +1,11 @@
 from tkinter import *
 
 foablak = Tk()
-
 foablak.geometry('900x500')
+foablak.title("Síkidomok")
+foablak.resizable(False, False)
+img = PhotoImage(file="matek-icon.png")
+foablak.iconphoto(True, img)
+foablak.configure(bg="grey")
+
 foablak.mainloop()
