d11b0654f629a8f1ec1e7fd51607c938145f3809
Viktor773 <viktor.varga2005@freemail.hu>
2022-05-16 13:22:48 +0200

    Fajl gomb elkészítése a menüsorból

--- sikidomok.py
+++ sikidomok.py
@@ -8,4 +8,23 @@ img = PhotoImage(file="matek-icon.png")
 foablak.iconphoto(True, img)
 foablak.configure(bg="grey")
 
+#MENU
+menusor = Frame(foablak)
+menusor.pack(side=TOP, fill=X)
+
+# def nevjegy():
+#     masodikAblak = Toplevel(foablak)
+#     fejlesztok = Message(masodikAblak, text="Készítők: ")
+
+#fajl
+menu1=Menubutton(menusor, text='Fájl')
+menu1.pack(side=LEFT)
+FAJL = Menu(menu1)
+FAJL.add_command(label="Névjegy", command=nevjegy)
+FAJL.add_command(label="Kilépés", command=foablak.destroy)
+
+
+#síkidomok
+
+
 foablak.mainloop()
